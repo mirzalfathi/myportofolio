8df453c2eea4a2e379cb73b11f2ce5915a0bfb4d
Mirza <mirzaaf2011@gmail.com>
2026-09-14 20:48:16 +0700

    fix: Development PWS

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,9 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from main.views import show_main, show_experience, show_education
 
 app_name = "main"
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
     path("education/", show_education, name="show_education"),
